aef981d679c24edfe125d40b8b80e0b9e44e96bc
ztcccc <24951859+ztcccc@users.noreply.github.com>
2019-05-15 21:44:25 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
+define('APIKEY', 'AIzaSyB8ZsXT8X2i5XlanON1zIe7YT4gDnqCMZA');//这里改成您的api key
+define('GJ_CODE', 'TW');//国家代码
+define('SITE_NAME', 'you');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EMAIL', 'heiiu@mailsac.com');//邮箱
 ?>
